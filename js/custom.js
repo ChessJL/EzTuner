bec0ca2f2cc45d16c804a3bf136786a856e2e40a
JerryLee <leechiyin1990@gmail.com>
2019-10-01 00:28:09 +0930

    Refine the indicator
    1. Add arrows into it
    2. Ignore the background sound effect
    3. Add some command for code

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,16 @@ var gs5=document.getElementById("gs5");
 var gs6=document.getElementById("gs6");
 var gs=document.getElementsByClassName("gs");
 
+// each bin equal to 22050/16384 = 1.346 Hz
+
+//array information['str string name',
+//                  'the lowest correct frequency'
+//                  'the highest correct frequency'
+//                  'the lowest bin location in a range for analyzing'
+//                  'the highest bin location in a range for analyzing'
+//                  'music note of the string'
+//                  ]
+
 var freq_gs1 =['gs1',328.5,331,234,254,'E1'];   //329.6
 var freq_gs2 =['gs2',245,247.5,173,193,'B2'];   //246.9
 var freq_gs3 =['gs3',195.5,197.5,135,155,'G3']; //196
@@ -14,7 +24,7 @@ var freq_gs5 =['gs5',109.5,111,71,91,'A5'];     //110
 var freq_gs6 =['gs6',81.41,83.41,51,71,'E6'];   //82.41
 
 var selected = [0,0,0,0,0,0];
-var correct;
+var loudestFreq;
 
 gs1.addEventListener("click",selectstring);
 gs2.addEventListener("click",selectstring);
@@ -23,28 +33,48 @@ gs4.addEventListener("click",selectstring);
 gs5.addEventListener("click",selectstring);
 gs6.addEventListener("click",selectstring);
 
-gs1.addEventListener("click", check);
-gs2.addEventListener("click", check);
-gs3.addEventListener("click", check);
-gs4.addEventListener("click", check);
-gs5.addEventListener("click", check);
-gs6.addEventListener("click", check);
 
-// pitch frequency
-function checkchord(){
+gs1.addEventListener("click", readstringinfo);
+gs2.addEventListener("click", readstringinfo);
+gs3.addEventListener("click", readstringinfo);
+gs4.addEventListener("click", readstringinfo);
+gs5.addEventListener("click", readstringinfo);
+gs6.addEventListener("click", readstringinfo);
+
+// Indicator
+
+function indicator(){
+  //if the pitch is correct, it shows green color.
   if(getLoudestFrequency(selected[3],selected[4])<selected[2] && getLoudestFrequency(selected[3],selected[4])>selected[1]){
-    correct = true;
-    return selected[5];
-  }else if (selected[5]!== 0 ) {
-    correct = false;
-    return selected[5];
-  }else{
-    return "Select a string";
+    fill(3, 255, 1);
+    text('<',235,50);
+    text(selected[5],150,50);
+    text('>',65,50);
+  }else if (selected[5]!== 0 ) { //if the pitch is not correct, and the tuner do not detect any sound.
+    fill(255);
+    text(selected[5],150,50);
+    text('<',235,50);
+    text('>',65,50);
+    if(getLoudestFrequency(selected[3],selected[4])>selected[2]){ //change color of the arrows if the pitch is too high.
+      fill(220,53,69);
+      text('<',235,50);
+      fill(255);
+      text('>',65,50);
+    }else if (getLoudestFrequency(selected[3],selected[4])<selected[1]) { //change color of the arrows if the pitch is too low.
+      fill(255);
+      text('<',235,50);
+      fill(220,53,69);
+      text('>',65,50);
+    }
+  }else{   // if user has not choosen any string.
+    fill(255);
+    text("Select a string",150,50);
   }
 }
 
 //Check user is clicking Guitar string or Ukulele string.
-function check(){
+
+function readstringinfo(){
   if(this.className === 'gs'){
     if(this.id === freq_gs1[0]){
       selected = freq_gs1;
@@ -68,23 +98,26 @@ function check(){
 
   }
 }
+
 //Find the loudest Frequency
 
 function getLoudestFrequency(low,high) {
     var halfsampleRate = sampleRate() / 2; // 22050
-    var spectrum = fft.analyze(); // array of amplitudes in bins
-    var bins = spectrum.length;  // 16384 bins
+    var spectrum = fft.analyze();               // array of amplitudes in bins
+    var bins = spectrum.length;                 // 16384 bins
     var maxAmp = 0;
     var largestBin;
   // each bin equal to 22050/16384 = 1.346 Hz
-    for (var i = low; i < high; i++) { //for loop to find the largest amplitude of bin
-        var thisAmp = spectrum[i]; // amplitude of current bin
+    for (var i = low; i < high; i++) {          //for loop to find the largest amplitude of bin
+        var thisAmp = spectrum[i];              // amplitude of current bin
         if (thisAmp > maxAmp) {
             maxAmp = thisAmp;
-            largestBin = i; //store the location of largestbin
+            if(maxAmp>100){                     //ignore the background sound effect
+              largestBin = i;                   //store the location of largestbin
+            }
         }
     }
-    var loudestFreq = largestBin * (halfsampleRate / bins);
+    loudestFreq = largestBin * (halfsampleRate / bins);
     return loudestFreq;
 }
 
